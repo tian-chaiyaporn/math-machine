7e0a2724ef463e5dc50ff12f55c93a9232833b43
Joel Wallis Jucá <joelwallis@gmail.com>
2017-06-09 18:19:19 -0300

    Subtraction operations

--- test/index.test.js
+++ test/index.test.js
@@ -9,6 +9,15 @@ assert.doesNotThrow(() => {
 	math(1, 2, '+');
 });
 
+// sums two numbers when used correctly
+assert.equal(math(10, 5, '-'), 5);
+
+// does not throw errors when used correctly
+assert.doesNotThrow(() => {
+	math(1, 2, '+');
+	math(1, 2, '-');
+});
+
 // throws an erro when NOT used correctly
 assert.throws(() => {
 	math(2, 3);
